374875738dfa2af2c59cf7c109e44dceb081ebeb
Sergey Stadnik <sstadnik@odecee.com.au>
2016-08-17 23:31:03 +1000

    Completed lesson 01-11

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component to produce some html
+const App = () => {
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take those components and put them on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
