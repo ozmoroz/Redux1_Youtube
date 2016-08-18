986c1129cdccc2f3034b1dd889d4ce2bc657854e
Sergey Stadnik <sstadnik@odecee.com.au>
2016-08-19 08:40:59 +1000

    Completed lesson 01-14

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCGNVl4M5HY8C7sb8zDSUOz9SA4oN2hoNQ';
 
 // Create a new component to produce some html
 const App = () => {
-  return <div>Hi!</div>;
+  return  (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take those components and put them on the page (in the DOM)
