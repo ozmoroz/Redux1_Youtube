b645fe08d6a7ca8f8b7791ae303d1a0c673f002b
Sergey Stadnik <sstadnik@odecee.com.au>
2016-08-20 11:18:05 +1000

    Completed lesson 02 - 023

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
 
 const API_KEY = 'AIzaSyCGNVl4M5HY8C7sb8zDSUOz9SA4oN2hoNQ';
 
 class App extends Component {
   constructor (props) {
     super(props);
+    //console.log('props: ', props);
     this.state = { videos: [] };
 
     YTSearch({key: API_KEY, term: 'windsurfing boards'}, videos => {
@@ -19,6 +22,7 @@ class App extends Component {
     return  (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
