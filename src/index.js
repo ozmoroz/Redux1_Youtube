a3de4cd1c4ae0f8ef50f09ba2cc0bb92efd3918c
Sergey Stadnik <sstadnik@odecee.com.au>
2016-08-25 18:54:06 +1000

    Finished lesson 02 - 31

--- src/index.js
+++ src/index.js
@@ -14,8 +14,11 @@ class App extends Component {
       selectedVideo: null,
       videos: []
     };
+    this.videoSearch('windsurfing boards');
+  }
 
-    YTSearch({key: API_KEY, term: 'windsurfing boards'}, videos => {
+  videoSearch (term) {
+    YTSearch({key: API_KEY, term: term}, videos => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -26,10 +29,10 @@ class App extends Component {
   render () {
     return  (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
-          onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
           videos={this.state.videos} />
       </div>
     );
